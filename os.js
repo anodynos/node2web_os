0c84ab707f729240de1d2d43adadfe13ae0a0d71
anodynos <agelos.pikoulas@gmail.com>
2014-02-01 18:00:26 +0200

    version v0.10.12

---
+++ os.js
@@ -0,0 +1,59 @@
+/** **nodejs 'os' core module.**
+
+From [node2web](http://github.com/anodynos/node2web) collection,
+via [browserify-CDN](http://wzrd.in/),
+exposed as 'os' to [bower](http://bower.io) for *browser* usage.
+
+Latest [browserify-CDN](http://wzrd.in/) reported **version: 'v0.10.12'** 
+**/
+!function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.os=e():"undefined"!=typeof global?global.os=e():"undefined"!=typeof self&&(self.os=e())}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.endianness = function () { return 'LE' };
+
+exports.hostname = function () {
+    if (typeof location !== 'undefined') {
+        return location.hostname
+    }
+    else return '';
+};
+
+exports.loadavg = function () { return [] };
+
+exports.uptime = function () { return 0 };
+
+exports.freemem = function () {
+    return Number.MAX_VALUE;
+};
+
+exports.totalmem = function () {
+    return Number.MAX_VALUE;
+};
+
+exports.cpus = function () { return [] };
+
+exports.type = function () { return 'Browser' };
+
+exports.release = function () {
+    if (typeof navigator !== 'undefined') {
+        return navigator.appVersion;
+    }
+    return '';
+};
+
+exports.networkInterfaces
+= exports.getNetworkInterfaces
+= function () { return {} };
+
+exports.arch = function () { return 'javascript' };
+
+exports.platform = function () { return 'browser' };
+
+exports.tmpdir = exports.tmpDir = function () {
+    return '/tmp';
+};
+
+exports.EOL = '\n';
+
+},{}]},{},[1])
+(1)
+});
+;
---
+++ readme.md
@@ -0,0 +1,7 @@
+**nodejs 'os' core module.**
+
+From [node2web](http://github.com/anodynos/node2web) collection,
+via [browserify-CDN](http://wzrd.in/),
+exposed as 'os' to [bower](http://bower.io) for *browser* usage.
+
+Latest [browserify-CDN](http://wzrd.in/) reported **version: 'v0.10.12'**
